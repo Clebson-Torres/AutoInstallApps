dcd55a67fc5d1977b5ab94e14baf99ac3f58b342
Clebson Torres <clebson.torres@gmail.com>
2022-07-24 16:05:41 -0300

    Created bot for download and install firefox

---
+++ main.py
@@ -0,0 +1,41 @@
+import pyautogui
+import time
+import pysimplegui
+
+pyautogui.hotkey ('win', 'd')
+pyautogui.press('win')
+time.sleep(2)
+pyautogui.write('brave')
+time.sleep(2)
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.write("https://www.mozilla.org/pt-BR/firefox/new/")
+time.sleep(2)
+pyautogui.press('enter')
+pyautogui.moveTo(1426, 130)
+time.sleep(1)
+pyautogui.click()
+pyautogui.moveTo(300, 760)
+time.sleep(2)
+pyautogui.click()
+pyautogui.moveTo(766, 561)
+time.sleep(3)
+pyautogui.click()
+time.sleep(5)
+pyautogui.moveTo(160, 979)
+pyautogui.click()
+time.sleep(5)
+pyautogui.moveTo(801, 694)
+pyautogui.click()
+
+
+
+
+
+#time.sleep(2)
+#pyautogui.click()
+#time.sleep(2)
+
+
+
+
